d12beb9feb7b62eecba54ed459f5b9b8730adf5d
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-08-16 02:07:36 +0900

    make CastExample.java

---
+++ ch2/src/CastExample.java
@@ -0,0 +1,12 @@
+// package ch2.src;
+
+public class CastExample {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        double c = (double)a / (double)b;
+
+        // System.out.println(a / b); => 2
+        System.out.println("cの値は " + c);
+    }
+}
