965a100c99d12fdb8ba78eb4a847a81fb1be6239
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-08-16 01:42:04 +0900

    update

--- ch2/src/CalcExample.java
+++ ch2/src/CalcExample.java
@@ -3,8 +3,17 @@ public class CalcExample {
         // System.out.println(2 + 3);
         // => 5
 
-        int i = 10;
-        int j = i * 2;
+        // int i = 10;
+        // int j = i * 2;
+        // System.out.println("jの値は " + j);
+
+        int i = 11;
+        i++;
+        i /= 2;
+        System.out.println("iの値は " + i);
+
+        int j;
+        j = i * i;
         System.out.println("jの値は " + j);
     }
 }
