850fa2a5f53025653b986ea2044759944065aacf
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-08-16 02:03:37 +0900

    make TypeExample3.java

---
+++ ch2/src/TypeExample3.java
@@ -0,0 +1,11 @@
+// package ch2.src;
+
+public class TypeExample3 {
+    public static void main(String[] args) {
+        int i = 10;
+        double d = i;
+
+        System.out.println("iの値は " + i);
+        System.out.println("dの値は " + d);
+    }
+}
