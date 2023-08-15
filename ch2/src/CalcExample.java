f46fef4fe836e096e6ca987215faa872e2ff577f
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-08-16 01:37:46 +0900

    make CalcExample.java

---
+++ ch2/src/CalcExample.java
@@ -0,0 +1,10 @@
+public class CalcExample {
+    public static void main(String[] args) {
+        // System.out.println(2 + 3);
+        // => 5
+
+        int i = 10;
+        int j = i * 2;
+        System.out.println("jの値は " + j);
+    }
+}
