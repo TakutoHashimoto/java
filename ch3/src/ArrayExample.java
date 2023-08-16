6388a08c40dd9bd8eaa527a52733be808bf4c796
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-08-16 21:00:20 +0900

    minor fix

--- ch3/src/ArrayExample.java
+++ ch3/src/ArrayExample.java
@@ -2,15 +2,17 @@
 
 public class ArrayExample {
     public static void main(String[] args) {
-        int[] scores = new int[5];
+        // int[] scores = new int[5];
 
-        scores[0] = 50;
-        scores[1] = 55;
-        scores[2] = 70;
-        scores[3] = 65;
-        scores[4] = 80;
+        // scores[0] = 50;
+        // scores[1] = 55;
+        // scores[2] = 70;
+        // scores[3] = 65;
+        // scores[4] = 80;
 
-        for (int i = 0; i < 5; i++) {
+        int[] scores = {50, 55, 70, 65, 80};
+
+        for (int i = 0; i < scores.length; i++) {
             System.out.println(scores[i]);
         }
     }
