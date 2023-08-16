192cc974fb34231a2e6253f08cafe896b826d356
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-08-16 19:16:40 +0900

    make NestExample.java

---
+++ ch3/src/NestExample.java
@@ -0,0 +1,13 @@
+// package ch3.src;
+
+public class NestExample {
+    public static void main(String[] args) {
+        for (int a = 1; a <= 3; a++) {
+            System.out.println("a = " + a);
+
+            for (int b = 1; b <= 3; b++) {
+                System.out.println("  b = " + b);
+            }
+        }
+    }
+}
