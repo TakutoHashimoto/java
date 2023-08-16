fba823ad7e3860c2e2d5f5faa28f803233bf4e49
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-08-17 03:14:22 +0900

    make StudentCard.java

---
+++ ch6/src/StudentCard.java
@@ -0,0 +1,10 @@
+class StudentCard {
+    int id;
+    String name;
+
+    StudentCard(int id, String name) {
+        System.out.println("StudentCardクラスのコンストラクタが呼び出されました");
+        this.id = id;
+        this.name = name;
+    }
+}
