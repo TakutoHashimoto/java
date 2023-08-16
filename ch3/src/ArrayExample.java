45fb0b2d6e84f6e4e037ac64d46c4d35baee57b9
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-08-16 20:58:30 +0900

    make ArrayExample.java

---
+++ ch3/src/ArrayExample.java
@@ -0,0 +1,17 @@
+// package ch3.src;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] scores = new int[5];
+
+        scores[0] = 50;
+        scores[1] = 55;
+        scores[2] = 70;
+        scores[3] = 65;
+        scores[4] = 80;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(scores[i]);
+        }
+    }
+}
