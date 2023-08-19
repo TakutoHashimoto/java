13d0fc5185b653c97f9637e9cdeeb3e2511ab2cd
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-08-20 02:55:16 +0900

    make ImportExample.java

---
+++ apply/ch1/src/ImportExample.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class ImportExample {
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        // 0~1のランダムな値を出力する
+        System.out.println(rand.nextDouble());
+    }
+}
