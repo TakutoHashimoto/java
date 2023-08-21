b68fcd2a211171ed6ce6817160173adb5a2c3679
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-08-21 12:21:41 +0900

    make FreeMemoryTest.java

---
+++ apply/ch4/src/FreeMemoryTest.java
@@ -0,0 +1,20 @@
+class DataSet {
+    int[] data = new int[100];
+}
+
+
+public class FreeMemoryTest {
+    public static void main(String[] args) {
+        System.out.println("空きサイズ: " + Runtime.getRuntime().freeMemory());
+
+        DataSet[] data = new DataSet[10000];
+
+        for (int i = 0; i < data.length; i++) {
+            data[i] = new DataSet();
+
+            if ((i+1) % 100 == 0) {
+                System.out.println("生成済みインスタンス数: " + (i+1) + " 空きメモリサイズ: " + Runtime.getRuntime().freeMemory());
+            }
+        }
+    }
+}
